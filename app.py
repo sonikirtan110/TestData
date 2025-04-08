c915646d5f61aff3292c9b6188c4716bc24fa6b0
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-04-08 10:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def predict():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, threaded=True)
+        
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
